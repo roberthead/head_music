83d26a552258b46fc3790e25638d6a28089abf9b
Rob Head <robert.head@gmail.com>
2017-03-27 18:28:09 -0700

    Assert that Fux CF entries score perfectly.

--- spec/head_music/style/rulesets/cantus_firmus_spec.rb
+++ spec/head_music/style/rulesets/cantus_firmus_spec.rb
@@ -4,9 +4,6 @@ describe HeadMusic::Style::Rulesets::CantusFirmus do
   FUX_EXAMPLES = [
     { key: 'D dorian', pitches: %w[D4 F4 E4 D4 G4 F4 A4 G4 F4 E4 D4] },
     { key: 'E phrygian', pitches: %w[E4 C4 D4 C4 A3 A4 G4 E4 F4 E4] },
-
-    # this one fails the RecoverLargeLeaps rule
-    # maybe change rule to allow spelling of the tonic chord?
     { key: 'F lydian', pitches: %w[F4 G4 A4 F4 D4 E4 F4 C5 A4 F4 G4 F4] },
   ]
 
@@ -24,6 +21,7 @@ describe HeadMusic::Style::Rulesets::CantusFirmus do
         end
 
         its(:fitness) { is_expected.to be > PENALTY_FACTOR }
+        its(:fitness) { is_expected.to eq 1 }
       end
     end
   end
