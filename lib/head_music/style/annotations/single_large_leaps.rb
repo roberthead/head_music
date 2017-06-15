a57fa7566f466036ccd12034a3edc271123d5e3c
Rob Head <robert.head@gmail.com>
2017-06-15 08:32:43 -0700

    Refactor more tolerant leap rule to subclass.

--- lib/head_music/style/annotations/single_large_leaps.rb
+++ lib/head_music/style/annotations/single_large_leaps.rb
@@ -1,17 +1,8 @@
 module HeadMusic::Style::Annotations
 end
 
-class HeadMusic::Style::Annotations::SingleLargeLeaps < HeadMusic::Style::Annotation
+class HeadMusic::Style::Annotations::SingleLargeLeaps < HeadMusic::Style::Annotations::RecoverLargeLeaps
   MESSAGE = "Recover leaps by step, repetition, opposite direction, or spelling triad."
-
-  def marks
-    melodic_intervals.drop(1).to_a.map.with_index do |interval, i|
-      previous_interval = melodic_intervals[i]
-      if unrecovered_leap?(previous_interval, interval, melodic_intervals[i+2])
-        HeadMusic::Style::Mark.for_all((previous_interval.notes + interval.notes).uniq)
-      end
-    end.compact
-  end
 
   private
 
@@ -21,15 +12,5 @@ class HeadMusic::Style::Annotations::SingleLargeLeaps < HeadMusic::Style::Annota
     return false if second_interval.step?
     return false if second_interval.repetition?
     !direction_changed?(first_interval, second_interval) && second_interval.leap?
-  end
-
-  def spelling_consonant_triad?(first_interval, second_interval, third_interval)
-    first_interval.spells_consonant_triad_with?(second_interval) ||
-      second_interval.spells_consonant_triad_with?(third_interval)
-  end
-
-  def direction_changed?(first_interval, second_interval)
-    first_interval.ascending? && second_interval.descending? ||
-      first_interval.descending? && second_interval.ascending?
   end
 end
