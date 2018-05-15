aa5b36daa2d488d9f423ac26741fd634e7f32165
Rob Head <robert.head@gmail.com>
2018-05-15 08:16:05 -0700

    Rename and move Spelling::EnharmonicEquivalence spec into directory
    - remove unnecessary inner class constructor specs.

--- spec/head_music/rudiments/pitch/enharmonic_equivalence_spec.rb
+++ spec/head_music/rudiments/pitch/enharmonic_equivalence_spec.rb
@@ -5,36 +5,6 @@ require 'spec_helper'
 describe HeadMusic::Pitch::EnharmonicEquivalence do
   subject(:enharmonic_equivalence) { described_class.get(pitch) }
 
-  describe 'constructor' do
-    context 'when passed a pitch' do
-      let(:pitch) { HeadMusic::Pitch.get('D5') }
-
-      its(:pitch_class) { is_expected.to eq HeadMusic::PitchClass.get('D') }
-      its(:pitch) { is_expected.to eq HeadMusic::Pitch.get('D5') }
-    end
-
-    context 'when passed a pitch string' do
-      let(:pitch) { 'D5' }
-
-      its(:pitch_class) { is_expected.to eq HeadMusic::PitchClass.get('D') }
-      its(:pitch) { is_expected.to eq HeadMusic::Pitch.get('D5') }
-    end
-
-    context 'when passed a pitch class' do
-      let(:pitch) { HeadMusic::PitchClass.get('D') }
-
-      its(:pitch_class) { is_expected.to eq HeadMusic::PitchClass.get('D') }
-      its(:pitch) { is_expected.to eq HeadMusic::Pitch.get('D4') }
-    end
-
-    context 'when passed a pitch class string' do
-      let(:pitch) { 'D' }
-
-      its(:pitch_class) { is_expected.to eq HeadMusic::PitchClass.get('D') }
-      its(:pitch) { is_expected.to eq HeadMusic::Pitch.get('D4') }
-    end
-  end
-
   describe '#equivalent?' do
     let(:pitch) { HeadMusic::Pitch.get('D#3') }
 
