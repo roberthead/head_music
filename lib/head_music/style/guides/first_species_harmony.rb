7c978849fbafcc2744e52cc23896a6a4ccb58685
Rob Head <robert.head@gmail.com>
2022-04-28 21:24:08 -0400

    Add TODOs for guildlines

--- lib/head_music/style/guides/first_species_harmony.rb
+++ lib/head_music/style/guides/first_species_harmony.rb
@@ -20,3 +20,11 @@ class HeadMusic::Style::Guides::FirstSpeciesHarmony
     RULESET.map { |rule| rule.new(voice) }
   end
 end
+
+# TODO: Guideline against leaping into P8 even by contrary motion.
+# TODO: Guideline allowing oblique motion < ~15% of the time.
+# TODO: Guideline against battuta (10-8 not at cadence, unless 10-8-6 voice exchange)
+# TODO: 16th C: No M6 leaps. m6 leaps ascending only.
+# TODO: 16th C (?): Avoid perfect consonances on consecutive downbeats
+# TODO: 16th C: No 5-7-1 ending
+# TODO: 16th C Cantus Firmus: Raise the 7 in the cadence, except in Phrygian
