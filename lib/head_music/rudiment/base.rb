69dc259cc08ddee082569c7e46e01337a2de4c29
Rob Head <robert.head@gmail.com>
2025-08-17 15:24:23 -0700

    Protect constructor of rudiment base class

--- lib/head_music/rudiment/base.rb
+++ lib/head_music/rudiment/base.rb
@@ -1,4 +1,9 @@
 module HeadMusic::Rudiment; end
 
 class HeadMusic::Rudiment::Base
+  private
+
+  def initialize
+    raise HeadMusic::AbstractMethodError, "Cannot instantiate abstract rudiment base class"
+  end
 end
--- spec/head_music/rudiment/base_spec.rb
+++ spec/head_music/rudiment/base_spec.rb
@@ -1,21 +1,7 @@
 require "spec_helper"
 
 describe HeadMusic::Rudiment::Base do
-  describe "instantiation" do
-    it "can be instantiated" do
-      expect { described_class.new }.not_to raise_error
-    end
-  end
-
-  describe "inheritance" do
-    it "is a class" do
-      expect(described_class).to be_a(Class)
-    end
-
-    it "provides a base for rudiment classes" do
-      test_class = Class.new(described_class)
-      instance = test_class.new
-      expect(instance).to be_a(described_class)
-    end
+  it "cannot be instantiated" do
+    expect { described_class.new }.to raise_error(HeadMusic::AbstractMethodError, "Cannot instantiate abstract rudiment base class")
   end
 end
