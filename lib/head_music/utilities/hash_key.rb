7edddff5c91e09fbb8b691cfa957b5f4c344ba55
Rob Head <robert.head@gmail.com>
2025-08-21 13:03:47 -0700

    Preserve alteration symbol in hash_key
    - fixes bug in test suite

--- lib/head_music/utilities/hash_key.rb
+++ lib/head_music/utilities/hash_key.rb
@@ -2,8 +2,40 @@
 module HeadMusic::Utilities; end
 
 # Util for converting an object to a consistent hash key
-module HeadMusic::Utilities::HashKey
+class HeadMusic::Utilities::HashKey
   def self.for(identifier)
-    I18n.transliterate(identifier.to_s).downcase.gsub(/\W+/, "_").to_sym
+    @hash_keys ||= {}
+    @hash_keys[identifier] ||= new(identifier).to_sym
+  end
+
+  attr_reader :original
+
+  def initialize(identifier)
+    @original = identifier
+  end
+
+  def to_sym
+    normalized_string.to_sym
+  end
+
+  private
+
+  def normalized_string
+    @normalized_string ||=
+      desymbolized_string.downcase.gsub(/\W+/, "_")
+  end
+
+  def desymbolized_string
+    transliterated_string
+      .gsub("𝄫", "_double_flat")
+      .gsub("♭", "_flat")
+      .gsub("♮", "_natural")
+      .gsub("♯", "_sharp")
+      .gsub("#", "_sharp")
+      .gsub("𝄪", "_double_sharp")
+  end
+
+  def transliterated_string
+    I18n.transliterate(original.to_s)
   end
 end
